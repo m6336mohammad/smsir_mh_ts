56527d3c8c86dc0a9291c4a8cd8ee758e981ef80
Mohammad hadi <m6336.programer@gmail.com>
2024-06-15 07:19:13 +0330

    rename to sampleUse.js

--- src/testSMS.ts
+++
@@ -1,9 +0,0 @@
-import { SendAlertSMSIR } from "./smsir_mh_ts";
-
-
-const api_key: string = '121212121211121';   ///Example API Key 
-const to: string = '09100059323';  //Example Phone Number
-const message: string = 'Hello,this test message!'; //Max 24 Character Length
-const templateId: string = "750016";  //Example template ID 
-
-SendAlertSMSIR(to, message, api_key, templateId);
